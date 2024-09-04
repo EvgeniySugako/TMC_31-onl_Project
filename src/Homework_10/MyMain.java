073512807b70b2897d6d464cd245bcc1a2a23996
ThinkPad <sugakoevgeniy95@gmail.com>
2024-09-04 22:58:14 +0300

    Homework_10

--- src/Homework_10/MyMain.java
+++ src/Homework_10/MyMain.java
@@ -1,14 +1,33 @@
 package Homework_10;
-
+/*
+Задача 1:
+Создать класс для описания пользователя системы. Переопределить в классе методы toString,hashcode и equals.
+Создать несколько экземпляров класса с одним и тем же значением полей и сравнить с помощью метода equals.
+Задача *:
+Создать программу для реализации поверхностного и глубокого клонирования объекта класса User.
+Пусть на вход программе будет передаваться тип операции клонирования (поверхностное клонирование или глубокое),
+а также id юзера для клонирования
+ */
 public class MyMain {
     public static void main(String[] args) {
-        User user1 = new User(30, "Maxim");
-        User user2 = new User(30, "Maxim");
-        User user3 = new User(18, "Maxim");
+        User[] users = new User[] {
+                new User("Admin", "Vika"),
+                new User("Admin", "Vika"),
+                new User("User", "Vika")
+        };
 
-        System.out.println(user1);
 
-        System.out.println(user1.equals(user2));
-        System.out.println(user2.equals(user3));
+//        User user1 = new User("Admin", "Vika");
+//        User user2 = new User("Admin", "Vika");
+//        User user3 = new User("User", "Vika");
+
+        for (User user: users) {
+            System.out.println(user);
+            System.out.println("hashCode: " + user.hashCode());
+            System.out.println("------------------------");
+        }
+
+        System.out.println(users[0].equals(users[1]));
+        System.out.println(users[1].equals(users[2]));
     }
 }
--- src/Homework_10/User.java
+++ src/Homework_10/User.java
@@ -1,4 +1,31 @@
 package Homework_10;
 
+import java.util.Objects;
+
 public class User {
+    private String name;
+    private String roles;
+
+    public User(String roles, String name) {
+        this.name = name;
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return roles == user.roles && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, roles);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "Name = " + name + "; Roles = " + roles + '\'' +'}';
+    }
 }
